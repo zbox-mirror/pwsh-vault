42b396cdf35dfcf228e16680a58a37d6805007d3
z17cx <mail@z17.cx>
2022-09-18 01:24:41 +0300

    2022-09-17 22:24:41

--- Vault.Functions.ps1
+++ Vault.Functions.ps1
@@ -51,7 +51,7 @@ function Write-VaultMsg() {
 
   switch ( $Type ) {
     "HL" {
-      Write-Host "$($NL)--- $($Message)" -ForegroundColor Blue
+      Write-Host "$($NL)--- $($Message)".ToUpper() -ForegroundColor Blue
     }
     "I" {
       Write-Information -MessageData "$($Message)" -InformationAction "$($Action)"
